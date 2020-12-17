7e81df8b2175c31fe90608a98ba682437effc955
marina <marina@LAPTOP-2I6RG5EQ>
2020-12-17 17:50:46 +0200

    aa

--- SendReceive.cs
+++ SendReceive.cs
@@ -15,13 +15,19 @@ namespace ManagerServiceNet
         ManagersTimer t;
         Timer startDelayTimer1;
         Timer startDelayTimer2;
+        int count;
         private static Logger logger = LogManager.GetCurrentClassLogger();
+        private int startTimeoutSec;
+        private int startPollingTimeoutSec;
         public SendReceive() {
+            startTimeoutSec = int.Parse(ConfigurationManager.AppSettings["startTimeoutSec"]);
+            startPollingTimeoutSec = int.Parse(ConfigurationManager.AppSettings["startPollingTimeoutSec"]);
             m = new RabbitMQManagersApprove();
 
             t = new ManagersTimer();
-            //wait 30 sec before start
-            startDelayTimer1 = new Timer(1000);
+            //start after 1 sec (for imeddiate start stop)
+            count = 1;
+            startDelayTimer1 = new Timer(startTimeoutSec * 1000);
             startDelayTimer1.Elapsed += new ElapsedEventHandler(this.OnTimer1);
             startDelayTimer1.Start();
         }
@@ -32,22 +38,24 @@ namespace ManagerServiceNet
             startDelayTimer1.Stop();
             if (Init())
                 return;
-            //if not succeeded to start immediately wait 30 sec
-            startDelayTimer2 = new Timer(30000);
+            logger.Error("rejected to connect 1nd time");
+            //if not succeeded to start immediately wait 1 minute
+            startDelayTimer2 = new Timer(startPollingTimeoutSec * 1000);
             startDelayTimer2.Elapsed += new ElapsedEventHandler(this.OnTimer2);
             startDelayTimer2.Start();
             
         }
 
+        //timer 2 never stops
+        //it tries to connect each 2 minutes
         public void OnTimer2(object sender, ElapsedEventArgs args) {
-            logger.Info("trying 2 to start connection to rabbitmq");
-            startDelayTimer2.Stop();
-            if (!Init()) {
-                logger.Error("rejected to connect 2nd time, stopping service");
-                throw new Exception("rejected to connect 2nd time, stopping service");
-            }
-                
+            count++;
+            logger.Info($"trying to start connection to rabbitmq {count} times");
+            if (Init())
+                return;
+            logger.Error($"rejected to connect {count} times");
         }
+
 
         private bool Init() {
             if (m.Init("") == true)
